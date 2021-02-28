c8e66f40aacc435cd7c5fc7e2fc86977267fccc0
YiChun Sung <ycsung.r@gmail.com>
2021-02-28 18:13:33 +0800

    Updated light.

--- src/pages/cube/index.js
+++ src/pages/cube/index.js
@@ -16,19 +16,21 @@ function CubePage() {
 
   const main = () => {
     let scene, renderer, camera, wall;
+    let sphereLightMesh, pointLight
+    let rotateAngle = 0
+    let cameraControl
 
     const init = () => {
       scene = new THREE.Scene();
-      // Renderer building.
+      // 渲染器設定
       renderer = new THREE.WebGLRenderer();
-      renderer.setSize(window.innerWidth, window.innerHeight); // 場景大小
+      renderer.setSize(window.innerWidth, window.innerHeight);
       renderer.setClearColor(0x1a1a1a, 1.0); // 預設背景顏色
-      renderer.shadowMap.enable = true; // 陰影效果
+      renderer.shadowMap.enabled = true;
+      renderer.shadowMap.type = 2; // THREE.PCFSoftShadowMap
 
-      // 將渲染器的 DOM 綁到網頁上
       cubContainer.current.appendChild(renderer.domElement);
-
-      // 建立相機
+      // 相機設定
       camera = new THREE.PerspectiveCamera(
         15,
         window.innerWidth / window.innerHeight,
@@ -38,16 +40,14 @@ function CubePage() {
       camera.position.set(20, 20, 40);
       camera.lookAt(scene.position);
 
-      let cameraControl = new OrbitControls(camera, renderer.domElement);
+      let axes = new THREE.AxesHelper(20);
+      scene.add(axes);
+
+      cameraControl = new OrbitControls(camera, renderer.domElement);
 
       cameraControl.enableDamping = true;
       cameraControl.dampingFactor = 0.25;
       cameraControl.enableZoom = true;
-
-      // 建立光源
-      let pointLight = new THREE.DirectionalLight(0xffffff, 2);
-      pointLight.position.set(-10, 20, 20)
-      scene.add(pointLight);
 
       const headMap = new THREE.TextureLoader()
         .load('/wall.jpg');
@@ -68,7 +68,7 @@ function CubePage() {
           map = headMap
         }
 
-        headMaterials.push(new THREE.MeshStandardMaterial({ map: map }))
+        headMaterials.push(new THREE.MeshLambertMaterial({ map: map }))
       }
 
       // 建立物體
@@ -83,7 +83,7 @@ function CubePage() {
       }); // 材質
 
       // 地板
-      const planeGeometry = new THREE.PlaneGeometry(10, 10, 32, 32); // 幾何體
+      const planeGeometry = new THREE.PlaneGeometry(30, 30, 32, 32); // 幾何體
       const planeMaterial = new THREE.MeshStandardMaterial({
         transparent: false,
         metalness: 0.8,
@@ -92,18 +92,71 @@ function CubePage() {
         map: grassMap
       }); // 材質
 
+      const planeMaterial2 = new THREE.MeshLambertMaterial({ color: 0x333333 })
+
       wall = new THREE.Mesh(geometry, headMaterials); // 建立網格物件
-      const plane = new THREE.Mesh(planeGeometry, planeMaterial);
+      let plane = new THREE.Mesh(planeGeometry, planeMaterial2);
       wall.position.set(0, 0, 0);
+      wall.castShadow = true;
+      
       plane.position.set(0, -1, 0);
+      plane.receiveShadow = true;
       plane.rotation.x = -0.5 * Math.PI;
-      scene.add(wall);
+      
       scene.add(plane);
+      scene.add(wall);
+      
       setCube(wall);
+      
+      // 建立光源
+      let ambientLight = new THREE.AmbientLight(0x404040);
+      scene.add(ambientLight);
+
+      // 設置聚光燈幫忙照亮物體
+      let spotLight = new THREE.SpotLight(0xf0f0f0);
+      spotLight.position.set(-20, 40, 20);
+      // spotLight.castShadow = true
+      scene.add(spotLight);
+
+      // 移動點光源
+      pointLight = new THREE.PointLight(0xccffcc, 1, 100); // 顏色, 強度, 距離
+      pointLight.castShadow = true; // 投影
+      pointLight.position.set(-30, 30, 30)
+      scene.add(pointLight);
+
+      
+
+      // 小球體模擬點光源實體
+      const sphereLightGeo = new THREE.SphereGeometry(5, 32, 32);
+      const sphereLightMat = new THREE.MeshBasicMaterial({ color: 0xff6600 });
+      sphereLightMesh = new THREE.Mesh(sphereLightGeo, sphereLightMat);
+      sphereLightMesh.position.set(-30, 30, 30)
+      sphereLightMesh.castShadow = true;
+      sphereLightMesh.position.y = 5;
+      scene.add(sphereLightMesh);
+
+    }
+
+    const pointLightAnimation = () => {
+      if (rotateAngle > 2 * Math.PI) {
+        rotateAngle = 0 // 超過 360 度後歸零
+      } else {
+        rotateAngle += 0.03 // 遞增角度
+      }
+
+      // 光源延橢圓軌道繞 Y 軸旋轉
+      sphereLightMesh.position.x = 8 * Math.cos(rotateAngle);
+      sphereLightMesh.position.z = 4 * Math.sin(rotateAngle);
+      console.log(8 * Math.cos(rotateAngle));
+      console.log(4 * Math.cos(rotateAngle));
+      // 點光源位置與球體同步
+      pointLight.position.copy(sphereLightMesh.position);
     }
 
     const render = () => {
+      cameraControl.update();
       // animate();
+      // pointLightAnimation();
       requestAnimationFrame(render);
       renderer.render(scene, camera);
     }
